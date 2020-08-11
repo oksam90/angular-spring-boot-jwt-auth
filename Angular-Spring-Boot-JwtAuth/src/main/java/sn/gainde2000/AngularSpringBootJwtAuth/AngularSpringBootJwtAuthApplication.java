93d8dc06fe96564af8740d6dd45b9e984bd64e25
Oksam90 <nzoenguioksam@gmail.com>
2020-08-11 15:09:16 +0000

    Le premier commit du projet

---
+++ Angular-Spring-Boot-JwtAuth/src/main/java/sn/gainde2000/AngularSpringBootJwtAuth/AngularSpringBootJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package sn.gainde2000.AngularSpringBootJwtAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootJwtAuthApplication.class, args);
+	}
+
+}
---
+++ Angular-Spring-Boot-JwtAuth/src/test/java/sn/gainde2000/AngularSpringBootJwtAuth/AngularSpringBootJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.gainde2000.AngularSpringBootJwtAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
